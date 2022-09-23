b184b6df0b5d3f1d316de6a657b8611f6ca0c5ce
venkatesansham <venkatesanben0gmail.com>
2022-09-23 08:33:24 +0530

    first commit venky code

---
+++ src/org/git/com/Gitclassss.java
@@ -0,0 +1,12 @@
+package org.git.com;
+
+public class Gitclassss {
+	private void sam() {
+		System.out.println("sam's code---------");
+
+		}
+		private void mano() {
+			System.out.println("mano's code-------------");
+
+		}
+}
